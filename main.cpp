df8bf5b9e3d09dbade14ab168c3eda21b61dc2c7
bebruh69 <aprilcheetah@gmail.com>
2021-09-28 08:42:41 +0300

    start

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Matrix
+{
+    int num_of_lines;
+    int num_of_columns;
+public:
+    Matrix(int num_)
+};
+
+int main()
+{
+    \
+    return 0;
+}
