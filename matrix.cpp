a986c1311fe80effe61f0b3cae24baf3541199db
bebruh69 <aprilcheetah@gmail.com>
2021-09-29 09:35:06 +0300

    added: standard init

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,24 @@
 #include "matrix.h"
 
-matrix::matrix()
-{
+Matrix::Matrix (int size_x, int size_y, initializer_list<initializer_list<int>> matrix)
+    {
+    size_x_ = size_x;
+    size_y_ = size_y;
 
-}
+    matrix_ = new int*[size_y_];
+    for (int i = 0; i < size_y_; i++)
+        matrix_[i] = new int [size_x_];
+
+    int y = 0, x = 0;
+
+    for (auto row: matrix)
+        {
+        for (auto elem: row)
+            {
+            matrix_[y][x] = elem;
+            x++;
+            }
+        x = 0;
+        y++;
+        }
+    }
--- matrix.h
+++ matrix.h
@@ -2,10 +2,39 @@
 #define MATRIX_H
 
 
-class matrix
-{
-public:
-    matrix();
-};
+#pragma once
+#include <initializer_list>
+#include <ostream>
+
+using namespace std;
+
+class Matrix
+    {
+    public:
+
+        int size_x_, size_y_;
+        int** matrix_;
+
+        //Matrix (int size_x, int size_y, initializer_list<initializer_list<int>> matrix);
+        //Matrix (int size_x, int size_y, int** matrix);
+        //Matrix (int size_x, int size_y);
+        //Matrix ();
+        //Matrix (const Matrix &matrix1);
+        //~Matrix ();
+
+        //Matrix& Tran ();
+        //bool is_right_step();
+        //bool is_left_step();
+
+        //Matrix& operator= (const Matrix &matrix);
+
+        //friend Matrix operator+ (const Matrix &lhs, const Matrix &rhs);
+        //friend Matrix operator- (const Matrix &lhs, const Matrix &rhs);
+        //friend Matrix operator* (const Matrix &lhs, const Matrix &rhs);
+        //friend Matrix operator* (const Matrix &lhs, const int rhs);
+        //friend ostream& operator<< (ostream &out, const Matrix &matrix);
+
+
+    };
 
 #endif // MATRIX_H
